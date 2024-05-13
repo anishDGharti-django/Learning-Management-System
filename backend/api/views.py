2769c17332a08cb5fd26353f5dd9cedb4350873d
Anish Gharti <anishDGharti@gmail.com>
2024-05-13 19:58:51 +0545

    car crud api

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.exceptions import NotFound
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
-
+from decimal import Decimal
+from rest_framework import status
 from api import serializers as api_serializers
 from userauths.models import User
 from api.utils import createOtp, sendMail
@@ -141,7 +142,7 @@ class CourseListAPIView(generics.ListAPIView):
 
 
 
-class CourseDetailApiView(generics.RetrieveAPIView):
+class CourseDetailAPIView(generics.RetrieveAPIView):
     serializer_class= api_serializers.CourseSerializer
     permission_classes = [AllowAny]
     queryset = api_models.Course.objects.filter(platform_status="Published", teacher_course_status="Published")
@@ -151,3 +152,88 @@ class CourseDetailApiView(generics.RetrieveAPIView):
         slug = self.kwargs['slug'] 
         course = api_models.Course.objects.get(slug=slug, platform_status="Published", teacher_course_status="Published")   
         return course
+    
+
+class CartAPIView(generics.CreateAPIView):
+    queryset = api_models.Cart.objects.all()
+    serializer_class = api_serializers.CartSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        course_id = request.data['course_id']  
+        user_id = request.data['user_id']
+        price = request.data['price']
+        country_name = request.data['country_name']
+        cart_id = request.data['cart_id']
+
+        print("course_id ==========", course_id)
+
+        course = api_models.Course.objects.filter(id=course_id).first()
+        
+        if user_id != "undefined":
+            user = User.objects.filter(id=user_id).first()
+        else:
+            user = None
+
+        try:
+            country_object = api_models.Country.objects.filter(name=country_name).first()
+            country = country_object.name
+        except:
+            country_object = None
+            country = "United States"
+
+        if country_object:
+            tax_rate = country_object.tax_rate / 100
+        else:
+            tax_rate = 0
+
+        cart = api_models.Cart.objects.filter(cart_id=cart_id, course=course).first()
+
+        if cart:
+            cart.course = course
+            cart.user = user
+            cart.price = price
+            cart.tax_fee = Decimal(price) * Decimal(tax_rate)
+            cart.country = country
+            cart.cart_id = cart_id
+            cart.total = Decimal(cart.price) + Decimal(cart.tax_fee)
+            cart.save()
+
+            return Response({"message": "Cart Updated Successfully"}, status=status.HTTP_200_OK)
+
+        else:
+            cart = api_models.Cart()
+
+            cart.course = course
+            cart.user = user
+            cart.price = price
+            cart.tax_fee = Decimal(price) * Decimal(tax_rate)
+            cart.country = country
+            cart.cart_id = cart_id
+            cart.total = Decimal(cart.price) + Decimal(cart.tax_fee)
+            cart.save()
+
+            return Response({"message": "Cart Created Successfully"}, status=status.HTTP_201_CREATED)
+
+
+
+
+class CartListAPIView(generics.ListAPIView):
+    serializer_class = api_serializers.CartSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        cart_id = self.kwargs['cart_id']
+        queryset = api_models.Cart.objects.filter(cart_id=cart_id)
+        return queryset
+    
+
+class CartItemDeleteAPIView(generics.DestroyAPIView):
+    serializer_class = api_serializers.CartSerializer
+    permission_classes = [AllowAny]
+
+    def get_object(self):
+        cart_id = self.kwargs['cart_id']
+        item_id = self.kwargs['item_id']
+
+        return api_models.Cart.objects.filter(cart_id=cart_id, id=item_id).first()
